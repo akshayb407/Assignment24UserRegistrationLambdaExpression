ad77a15194b2869a64b203aba6c5f839a1e6adea
akshayb407 <akshayb407@gmail.com>
2022-09-04 15:43:28 +0530

    UC1_Valid_First_Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
-    internal class Program
+    class Program
     {
+        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
+            FirstNameVal();
+        }
+        public static void FirstNameVal()
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            Console.Write("Enter First Name: ");
+            string first_name = Console.ReadLine();
+            bool validate = rg.IsMatch(first_name);
+            if (validate)
+                Console.WriteLine("Name is Valid : " + first_name);
+            else
+                Console.WriteLine("Invalid FirstName");
+
         }
     }
 }
